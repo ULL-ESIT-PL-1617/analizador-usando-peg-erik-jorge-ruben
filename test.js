2786c255d2d1d861b0f6302400a5e63197ce5c51
Erik Barreto de Vera <zephyr@Eriks-iMac-2.local>
2017-05-07 18:14:04 +0100

    avanzando los tests

--- test.js
+++ test.js
@@ -6,22 +6,39 @@ function removeSpaces(str){
   return str.replace(/\s/g,'');;
 }
 
-describe("Expresiones aritméticas", function () {
-  it("# a := 5 + 5", function(){
-    var tree = `[{"type": ":=", "left": {"type": "ID", "value": "a"}, "right": [{"type": "+", "left":
-                  {"type": "NUM", "value": 5}, "right": {"type": "NUM", "value": 5} }] }]
+describe("<---Test para comprobar que se cumple la salida esperada--->", function () {
+  it("# a := 2 + 2", function(){
+    var tree = `
+                {"constantes":{},"variables":{},"procedimientos":{},"funciones":{},"sentencias":{"type":"assign","left":"a","right":{"type":"+","left":2,"right":2}}}
                 `;
     tree = removeSpaces(tree);
-    var res = removeSpaces(JSON.stringify(PEG.parse('a := 5 + 5;'), null, 2))
+    var res = removeSpaces(JSON.stringify(PEG.parse('a := 2 + 2'), null, 2))
     expect(res).to.equal(tree);
   });
-  it("# a = 5 * 5 - 10", function(){
-    var tree = `[{"type": "=", "left": {"type": "ID","value": "a" },"right": [{"type": "-","left": [{
-                "type": "*","left": { "type": "NUM", "value": 5},"right": {"type": "NUM", "value": 5}
-                }],"right": {"type": "NUM", "value": 10} } ]}]
+  
+  it("# if a < 10 then a := 15 else a := 45", function(){
+    var tree = `
+                {"constantes":{},"variables":{},"procedimientos":{},"funciones":{},"sentencias":{"type":"if","condition":{"type":"<","left":"a","right":10},"then":{"type":"assign","left":"a","right":15},"else":{"type":"assign","left":"a","right":45}}}
                 `;
     tree = removeSpaces(tree);
-    var res = removeSpaces(JSON.stringify(PEG.parse('a = 5 * 5 - 10;'), null, 2))
+    var res = removeSpaces(JSON.stringify(PEG.parse('if a < 10 then a := 15 else a := 45'), null, 2))
     expect(res).to.equal(tree);
   });
+  it("# while a <= 10 do begin a := a + 2; b := b + 2 end", function(){
+    var tree = `
+                {"constantes":{},"variables":{},"procedimientos":{},"funciones":{},"sentencias":{"type":"while","condition":{"type":"<=","left":"a","right":10},"do":{"type":"block","code":{"1":{"type":"assign","left":"a","right":{"type":"+","left":"a","right":2}},"2":{"type":"assign","left":"b","right":{"type":"+","left":"b","right":2}}}}}}
+                `;
+    tree = removeSpaces(tree);
+    var res = removeSpaces(JSON.stringify(PEG.parse('while a <= 10 do begin a := a + 2; b := b + 2 end'), null, 2))
+    expect(res).to.equal(tree);
+  });
+  it("# function funcId (a, b, c){  const d = 1, e = 2;  var f, g, h;  begin    f := a + d;    g := b + e  end }; a := funcId(2, b, 2+2)", function(){
+    var tree = `
+                {"constantes":{},"variables":{},"procedimientos":{},"funciones":{},"sentencias":{"type":"while","condition":{"type":"<=","left":"a","right":10},"do":{"type":"block","code":{"1":{"type":"assign","left":"a","right":{"type":"+","left":"a","right":2}},"2":{"type":"assign","left":"b","right":{"type":"+","left":"b","right":2}}}}}}
+                `;
+    tree = removeSpaces(tree);
+    var res = removeSpaces(JSON.stringify(PEG.parse('function funcId (a, b, c){  const d = 1, e = 2;  var f, g, h;  begin    f := a + d;    g := b + e  end }; a := funcId(2, b, 2+2)'), null, 2))
+    expect(res).to.equal(tree);
+  });
+  
 });
